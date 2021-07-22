37904d64acf614553c08405c3152f581c5100570
Romain PIOT <romainpiot.pro@gmail.com>
2021-07-22 04:21:26 +0200

    update doctrine config

---
+++ back/src/Infrastructure/Symfony/Entity/Project.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Infrastructure\Symfony\Entity;
+
+use Infrastructure\Symfony\Repository\ProjectRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProjectRepository::class)
+ */
+class Project
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
