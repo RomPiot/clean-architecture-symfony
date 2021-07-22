6b95e3f185121d73d7fb412fd00d545515aa1810
Romain PIOT <romainpiot.pro@gmail.com>
2021-07-22 02:00:35 +0200

    clean architecture

---
+++ back/src/Domain/Entity/Project.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Domain\Entity;
+
+use Domain\Repository\ProjectRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProjectRepository::class)
+ */
+class Project
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
