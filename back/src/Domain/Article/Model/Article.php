a23de3ecf4080c8bfa98cab0124fcafb2d530b77
Romain PIOT <romainpiot.pro@gmail.com>
2021-07-27 00:57:35 +0200

    add setter & getter

--- back/src/Domain/Article/Model/Article.php
+++ back/src/Domain/Article/Model/Article.php
@@ -30,4 +30,124 @@ class Article
         $this->isPublished = $isPublished;
     }
 
+
+    /**
+     * Get the value of title
+     *
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set the value of title
+     *
+     * @param string $title
+     *
+     * @return self
+     */
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of content
+     *
+     * @return string
+     */
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set the value of content
+     *
+     * @param string $content
+     *
+     * @return self
+     */
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of createdAt
+     *
+     * @return DateTimeInterface
+     */
+    public function getCreatedAt(): DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set the value of createdAt
+     *
+     * @param DateTimeInterface $createdAt
+     *
+     * @return self
+     */
+    public function setCreatedAt(DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of updatedAt
+     *
+     * @return DateTimeInterface
+     */
+    public function getUpdatedAt(): DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * Set the value of updatedAt
+     *
+     * @param DateTimeInterface $updatedAt
+     *
+     * @return self
+     */
+    public function setUpdatedAt(DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of isPublished
+     *
+     * @return bool
+     */
+    public function getIsPublished(): bool
+    {
+        return $this->isPublished;
+    }
+
+    /**
+     * Set the value of isPublished
+     *
+     * @param bool $isPublished
+     *
+     * @return self
+     */
+    public function setIsPublished(bool $isPublished): self
+    {
+        $this->isPublished = $isPublished;
+
+        return $this;
+    }
 }
--- back/src/Domain/Category/Model/Category.php
+++ back/src/Domain/Category/Model/Category.php
@@ -12,17 +12,113 @@ class Category
 
     private DateTimeInterface $updatedAt;
 
-    protected bool $isActivate;
+    protected bool $isEnabled;
 
     public function __construct(
         string $name,
         DateTimeInterface $createdAt,
         DateTimeInterface $updatedAt,
-        bool $isActivate
+        bool $isEnabled
     ) {
         $this->name = $name;
         $this->createdAt = $createdAt;
         $this->updatedAt = $updatedAt;
-        $this->isActivate = $isActivate;
+        $this->isEnabled = $isEnabled;
+    }
+
+    /**
+     * Get the value of name
+     *
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @param string $name
+     *
+     * @return self
+     */
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of createdAt
+     *
+     * @return DateTimeInterface
+     */
+    public function getCreatedAt(): DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set the value of createdAt
+     *
+     * @param DateTimeInterface $createdAt
+     *
+     * @return self
+     */
+    public function setCreatedAt(DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of updatedAt
+     *
+     * @return DateTimeInterface
+     */
+    public function getUpdatedAt(): DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * Set the value of updatedAt
+     *
+     * @param DateTimeInterface $updatedAt
+     *
+     * @return self
+     */
+    public function setUpdatedAt(DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of isEnabled
+     *
+     * @return bool
+     */
+    public function getIsEnabled(): bool
+    {
+        return $this->isEnabled;
+    }
+
+    /**
+     * Set the value of isEnabled
+     *
+     * @param bool $isEnabled
+     *
+     * @return self
+     */
+    public function setIsEnabled(bool $isEnabled): self
+    {
+        $this->isEnabled = $isEnabled;
+
+        return $this;
     }
 }
